c99bc64b1815612f3422238ac4e33bab87478337
Kristóf Jakab <jaksi@jaksi.io>
2015-11-15 16:30:57 +0100

    Add show action to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
