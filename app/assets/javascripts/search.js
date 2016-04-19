d4385e23083bd00cca0fc571ac64bcedcc727a34
salierri <g.gergely314@gmail.com>
2016-04-19 16:05:44 +0200

    Cleaned out the search functionality

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,23 +1,66 @@
 $(document).ready(function () {
 
-	var input = document.querySelector('#search-field');
+    if(window.location.href.indexOf("/search") > -1) {
+        var query = getUrlParameter("query");
+        $("#search-field").val(query);
+        updateSuggestions(query);
+    }
 
-	var inputStream = Rx.Observable.fromEvent(input, "keyup")
-		.debounce(100)
-		.map(function () {
-			return input.value;
-		})
-		.distinctUntilChanged();
+    var input = document.querySelector('#search-field');
 
-	inputStream.subscribe(search);
+    var inputStream = Rx.Observable.fromEvent(input, "keyup")
+        .debounce(100)
+        .map(function () {
+            return input.value;
+        })
+        .distinctUntilChanged();
 
-	function search (query) {
-		$.ajax({
-			method: 'GET',
-			url: '/search/suggest',
-			data: {query: query}
-		}).done(function (resp) {
-			$('#suggestions').html(resp);
-		});
-	}
+    inputStream.subscribe(inputChange);
+
+    function updateSuggestions(query) {
+        if(query.length > 0) {
+            $.ajax({
+                method: 'GET',
+                url: '/search/suggest',
+                data: {query: query}
+            }).done(function (resp) {
+                $('#suggestions').html(resp);
+                document.title = "Keresés: " + query;
+                window.history.pushState({ "html" : resp.html, "pageTitle" : "Keresés: " + query },"", "/search?query=" + query);
+            });
+        }
+    }
+
+    function inputChange (query) {
+        if(window.location.href.indexOf("/search") > -1) {
+            updateSuggestions(query);
+        } else {
+            $.ajax({
+                method: 'GET',
+                url: '/search',
+                data: {query: query}
+            }).done(function (resp) {
+                $('#content-main').html($(resp).find("#content-main").html());
+
+                document.title = "Keresés";
+                window.history.pushState({ "html" : resp.html, "pageTitle" : "Keresés"},"", "/search?query=" + query);
+                updateSuggestions(query);
+            });
+        }
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
 });
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,36 +1,12 @@
 class SearchController < ApplicationController
-  def search 
-    x = 5
-    if params['query'].to_s != '' 
-      param = "%#{params['query']}%"
-      # @korok  User.find_by(usr_korok: param) TODO FIXME
-      @users_name =  User.where("usr_lastname  LIKE ? OR usr_firstname  LIKE ?", param, param).limit(50)
-      @UsersNameFirstX = @users_name[0..(x/2.0).round(0)-1]
-      @users_nick =  User.where("usr_nickname  LIKE ?", param).limit(50)
-      @UsersNickFirstX = @users_nick[0..(x/2.0).round(0)-1]
-      @others =  User.where("usr_dormitory  LIKE ? OR usr_room  LIKE ? OR usr_webpage  LIKE ? OR usr_email LIKE ? ",param ,param, param, param).limit(50) ## azért majd csak azt adjuk vissza amit publicra állított. #FIXME TODO: Kör leírásában is kereshetünk
-      @OthersFirstX = others = @others[0..x-1]
-      @phonenumbers = User.where("usr_cell_phone LIKE ?",param).limit(50)
-      @PhoneNumbersFirstX = @phonenumbers[0..x-1]
-
-## --------------- ----------------------------------------------- --------------------
-## Ezek itt lent még nem működnek
-'      groups = User.where("grp_name LIKE ? OR grp_description LIKE ?", param, param)
-      
-      @GroupsFirsX = groups.first(x)
-      @GruplsAll = groups
-'
-
-## Eddig nem működik
-## ---------------- ----------------------------------------------- --------------------
-    end
+  def search
+    
   end
 
   def suggest
     if params['query'].to_s != ''
-      param = "%#{params['query']}%"
-      @by_nick =  User.where("usr_nickname LIKE ?", param).limit(3)
-      @by_name =  User.where("(usr_lastname || ' ' || usr_firstname) LIKE ?", param).limit(3)
+      param = "%#{params['query']}%".downcase
+      @results =  User.where("lower(usr_lastname || ' ' || usr_firstname) LIKE ? OR lower(usr_nickname) LIKE ?", param, param).limit(10)
 
       render partial: 'suggest_partial'
     end
