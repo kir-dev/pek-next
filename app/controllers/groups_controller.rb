8ce68731e786d3e62bf3961349fc20050f036c24
salierri <g.gergely314@gmail.com>
2016-05-15 22:26:51 +0200

    Groups page now displays data from the db

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,18 +6,7 @@ class GroupsController < ApplicationController
   end
 
   def show
-    params[:group_id]
 
-    @group = {
-      name: "KIR fejlesztők és üzemeltetők",
-      type: "szakmai kör",
-      description: "A Villanykari Információs Rendszer fejlesztésével és üzemeltetésével foglalkozó kör.",
-      webpage: "http://kir-dev.sch.bme.hu",
-      maillist: "kir-dev@sch.bme.hu",
-      head: "KIR admin",
-      funded: 2011,
-      issvie: true,
-      users_can_apply: true      
-    }
+    @group = Group.find(params[:group_id])
   end
 end
