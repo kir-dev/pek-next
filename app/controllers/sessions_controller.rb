b419fa621f3b86eef32ea18414fcf255b4619f66
salierri <g.gergely314@gmail.com>
2016-06-22 19:32:57 +0200

    Registration backend cleaned up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    $user = request.env['omniauth.auth']['extra']['raw_info']
-    user = User.find_by usr_auth_sch_id: $user['internal_id']
+    oauth_data = request.env['omniauth.auth']['extra']['raw_info']
+    user = User.find_by usr_auth_sch_id: oauth_data['internal_id']
     if !user
+      session[:oauth_data] = oauth_data
       redirect_to '/register'
     else
       session[:user] = user.id
@@ -11,14 +12,14 @@ class SessionsController < ApplicationController
   end
 
   def register
-    user = User.create(usr_auth_sch_id: $user[:internal_id],
-      email: $user[:mail],
-      firstname: $user[:givenName], 
-      lastname: $user[:sn],
+    user = User.create(usr_auth_sch_id: session[:oauth_data]["internal_id"],
+      email: session[:oauth_data]["mail"],
+      firstname: session[:oauth_data]["givenName"], 
+      lastname: session[:oauth_data]["sn"],
       screen_name: request.params[:username],
       dormitory: request.params[:dormitory],
       room: request.params[:room])
-      session[:user] = user.id
-      redirect_to '/'
+    session[:user] = user.id
+    redirect_to '/'
   end
 end
