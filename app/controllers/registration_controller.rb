00c9cc86bdf89f299f16f4d316eb3be9d4556129
salierri <g.gergely314@gmail.com>
2016-07-12 23:23:04 +0200

    Exception handling durign registration

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -5,16 +5,35 @@ class RegistrationController < ApplicationController
   end
 
   def create_user
-    user = User.create(usr_auth_sch_id: session[:oauth_data]["internal_id"],
-      email: session[:oauth_data]["mail"],
-      firstname: session[:oauth_data]["givenName"], 
-      lastname: session[:oauth_data]["sn"],
-      screen_name: request.params[:username],
-      dormitory: request.params[:dormitory],
-      room: request.params[:room])
-    session[:user] = user.id
-    session.delete(:oauth_data)
-    redirect_to root_url
+    oauth_data = session[:oauth_data]
+    begin
+      user = User.create(usr_auth_sch_id: oauth_data["internal_id"],
+        email: oauth_data["mail"],
+        firstname: oauth_data["givenName"], 
+        lastname: oauth_data["sn"],
+        screen_name: request.params[:username],
+        dormitory: request.params[:dormitory],
+        room: request.params[:room])
+    rescue ActiveRecord::RecordNotUnique => e
+      @error = { expected: true }
+      if e.message.include? "users_usr_screen_name_key"
+        @error[:message] = "Ez a felhasználónév már foglalt!"
+      elsif e.message.include? "users_usr_auth_sch_id_key"
+        @error[:message] = "Ezzel az auth.sch id-val már regisztráltak!"
+      elsif e.message.include? "users_usr_bme_id_key"
+        @error[:message] = 'Ezzel a BME id-val már regisztráltak!'
+      end
+      new()
+      render :new
+    rescue ActiveRecord::ActiveRecordError => e
+      @error = { message: "Váratlan probléma történt!" }
+      new()
+      render :new
+    else
+      session[:user] = user.id
+      session.delete(:oauth_data)
+      redirect_to root_url
+    end
   end
 
 end
