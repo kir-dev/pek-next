9573d2f3a9aac3806d5e2c10c1297ca874c9b8c8
salierri <g.gergely314@gmail.com>
2016-10-16 18:08:44 +0200

    Refactored search javascript

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,30 +1,28 @@
 $(document).ready(function () {
 
-    if(window.location.href.indexOf("/search") > -1) {
-        var query = getUrlParameter("query");
+    if($('#suggestions').length > 0) {
+        var query = Helpers.getUrlParameter("query");
         $("#search-field").val(query);
         updateSuggestions(query);
     }
 
     var input = document.querySelector('#search-field');
 
-    var inputStream = Rx.Observable.fromEvent(input, "keyup")
-        .debounce(100)
-        .map(function () {
-            return input.value;
-        })
-        .distinctUntilChanged();
+    if(input != null) {
+        var inputStream = Rx.Observable.fromEvent(input, "keyup")
+            .debounce(100)
+            .map(function () {
+                return input.value;
+            })
+            .distinctUntilChanged();
 
-    inputStream.subscribe(inputChange);
+        inputStream.subscribe(inputChange);
+    }
 
     function updateSuggestions(query) {
         if(query.length > 0) {
-            $.ajax({
-                method: 'GET',
-                url: '/search/suggest',
-                data: {query: query}
-            }).done(function (resp) {
-                $('#suggestions').html(resp);
+            console.log(typeof query);
+            $('#suggestions').load('/search/suggest', $.param({query: query}), function (resp) {
                 document.title = "Keresés: " + query;
                 window.history.pushState({ "html" : resp.html, "pageTitle" : "Keresés: " + query },"", "/search?query=" + query);
             });
@@ -32,7 +30,7 @@ $(document).ready(function () {
     }
 
     function inputChange (query) {
-        if(window.location.href.indexOf("/search") > -1) {
+        if($('#suggestions').length > 0) {
             updateSuggestions(query);
         } else {
             $.ajax({
@@ -48,19 +46,4 @@ $(document).ready(function () {
             });
         }
     }
-
-    function getUrlParameter(sParam) {
-        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-            sURLVariables = sPageURL.split('&'),
-            sParameterName,
-            i;
-
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
-
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === undefined ? true : sParameterName[1];
-            }
-        }
-    };
 });
