db0f8d93e259d95613316d15afe94f7be93abf99
G <domok.gergely+github@gmail.com>
2016-10-16 21:00:54 +0200

    links to profiles from search

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,11 +1,16 @@
 class ProfileController < ApplicationController
 
   def show
-  	if session[:user]
-    	@user = User.find(session[:user])
+    if session[:user]
+      if params.key?(:number)
+        id = params[:number]
+      else
+        id = session[:user]
+      end
+      @user = User.find(id)
     else
-    	redirect_to '/auth/oauth'
+      redirect_to '/auth/oauth'
     end
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   resources :users
   get '/search', to: 'search#search'
   get '/search/suggest', to: 'search#suggest'
+  get '/profile/:number', to: 'profile#show'
+  get '/profile/', to: 'profile#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'profile#show'
+  root :to => redirect('/profile')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
