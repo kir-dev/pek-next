76f9a0062eedf594c530f730c7df97af0da7353e
salierri <g.gergely314@gmail.com>
2016-10-16 18:14:23 +0200

    Removed empty files and refactored partial

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,10 +6,10 @@ class SearchController < ApplicationController
   def suggest
     if params['query'].to_s != ''
       param = "%#{params['query']}%".downcase
-      @results =  User.where("lower(usr_lastname || ' ' || usr_firstname) LIKE ? OR lower(usr_nickname) LIKE ?\
+      results =  User.where("lower(usr_lastname || ' ' || usr_firstname) LIKE ? OR lower(usr_nickname) LIKE ?\
        OR lower(usr_email) LIKE ? OR usr_cell_phone LIKE ?", param, param, param, param).limit(10)
 
-      render partial: 'suggest_partial'
+      render partial: 'suggest', locals: {results: results}
     end
 
   end
