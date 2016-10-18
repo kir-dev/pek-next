8bf4375f6164cdcb048fef4e1387e7ee4b55f081
salierri <g.gergely314@gmail.com>
2016-10-18 12:31:22 +0200

    Further search query refactor

--- app/services/search_query.rb
+++ app/services/search_query.rb
@@ -4,25 +4,22 @@ class SearchQuery
   end
 
   def search(term)
-    terms = term.split
-    params = []
-    terms.each do |t|
-      params += ["%#{t}%".downcase] * query_for_one_keyword.count('?')
-    end
-    query = ([query_for_one_keyword] * terms.size).join(' AND ')
+    params = term.split.flat_map { |t| ["%#{t}%".downcase] * query_for_one_keyword.count('?') }
+    query = ([query_for_one_keyword] * term.split.size).join(' AND ')
     return User.where(query, *params).limit(10)
   end
 
   private
 
-  def query_for_one_keyword()
-    return "(lower(usr_lastname) LIKE ? \
-     OR lower(usr_firstname) LIKE ? \
-     OR lower(usr_nickname) LIKE ? \
-     OR lower(usr_email) LIKE ? \
-     OR usr_cell_phone LIKE ? \
-     OR lower(usr_dormitory) LIKE ?
-     OR usr_room LIKE ?)"
+  def query_for_one_keyword
+    <<-SQL.squish
+      (lower(usr_lastname) LIKE ?
+      OR lower(usr_firstname) LIKE ?
+      OR lower(usr_nickname) LIKE ?
+      OR lower(usr_email) LIKE ?
+      OR usr_cell_phone LIKE ?
+      OR lower(usr_dormitory) LIKE ?
+      OR usr_room LIKE ?)
+    SQL
   end
-
 end
