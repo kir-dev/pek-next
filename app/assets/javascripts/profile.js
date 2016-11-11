660fc65b7cdda2dd74a269d1f99dd631c4f2ffbe
salierri <g.gergely314@gmail.com>
2016-11-11 20:50:05 +0100

    Profile javascript to module pattern

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,35 +1,45 @@
 
-var cropper;
-$(document).ready(function () {
-    if($('#cropper-img').length > 0) {
-        cropper = new Cropper(document.getElementById('cropper-img'), {
-            aspectRatio: 1/1,
-            zoomable: false,
-            movable: false,
+var settings = (function() {
+    var module = {};
+    var cropper;
 
+    module.init = function() {
+        if($('#cropper-img').length > 0) {
+            cropper = new Cropper(document.getElementById('cropper-img'), {
+                aspectRatio: 1/1,
+                zoomable: false,
+                movable: false,
+
+            });
+        }
+        
+        $("#crop-finish-button").click(cropDone)
+    }
+
+    function cropDone() {
+        var croppedData = cropper.getCroppedCanvas({
+            width: 300,
+            height: 300
+        }).toDataURL();
+        $.ajax({
+            type: "POST",
+            url: "/settings/upload",
+            data: {
+                croppedData: croppedData
+            }
+        }).done(function (data) {
+            UIkit.notify({
+                message : 'Sikeres feltöltés!',
+                status  : 'info',
+                timeout : 5000,
+                pos     : 'top-center'
+            });
+            $('#image-upload-container').fadeOut();
+            setTimeout(function () {$('#back-button-container').fadeIn();}, 400);
         });
     }
-});
 
-function cropDone() {
-    var croppedData = cropper.getCroppedCanvas({
-        width: 300,
-        height: 300
-    }).toDataURL();
-    $.ajax({
-        type: "POST",
-        url: "/settings/upload",
-        data: {
-            croppedData: croppedData
-        }
-    }).done(function (data) {
-        UIkit.notify({
-            message : 'Sikeres feltöltés!',
-            status  : 'info',
-            timeout : 5000,
-            pos     : 'top-center'
-        });
-        $('#image-upload-container').fadeOut();
-        setTimeout(function () {$('#back-button-container').fadeIn();}, 400);
-    });
-}
+    return module;
+}());
+
+$(document).ready(settings.init);
