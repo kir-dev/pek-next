9a32d6936ed8d4cd4f3299c5d4d4273294675cca
salierri <g.gergely314@gmail.com>
2017-01-06 16:10:57 +0100

    Search query testing

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -7,8 +7,42 @@ class SearchControllerTest < ActionController::TestCase
   end
 
   test "empty search page" do
-    get 'search'
+    get :search
     assert_response :success
+  end
+
+  test "suggestion page response" do
+    get :suggest
+    assert_response :success
+  end
+
+  test "search for empty queue" do
+    query = SearchQuery.new.search("", nil)
+    assert_equal 10, query.size
+    assert_equal 11, query.first.usr_id
+  end
+
+  test "search in nickname" do
+    query = SearchQuery.new.search("sanyi", nil)
+    assert_equal 1, query.size
+    assert_equal 10, query.first.usr_id
+  end
+
+  test "search in last+firstname" do
+    query = SearchQuery.new.search("Kovács Béla", nil)
+    assert_equal 1, query.size
+    assert_equal 11, query.first.usr_id
+  end
+
+  test "next search page of generated users" do
+    query = SearchQuery.new.search("pék", 1)
+    assert_equal 10, query.size
+    assert_equal 289, query.first.usr_id
+  end
+
+  test "search for non-existent user" do
+    query = SearchQuery.new.search("gergely", nil)
+    assert_equal 0, query.size
   end
 
 end
