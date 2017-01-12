6cd210c4cc9ed3281db4dc02bcb47b55c28f9fe6
salierri <g.gergely314@gmail.com>
2017-01-12 22:59:14 +0100

    Testing will not require APP_ID and APP_SECRET

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if ENV['NONAUTH']
-  provider :oauth, ENV.fetch('APP_ID'), ENV.fetch('APP_SECRET')
+  provider :oauth, ENV.fetch('APP_ID'), ENV.fetch('APP_SECRET') unless Rails.env.test?
 end
