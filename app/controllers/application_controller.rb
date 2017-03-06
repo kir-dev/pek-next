29d2d7a64a3d1208bc959c518332e302eba45f85
Balazs Varga <varbalazs@gmail.com>
2017-03-06 19:26:42 +0100

    Remove Developer OmniAuth strategy from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  use OmniAuth::Strategies::Developer
 
   def require_login
     redirect_to oauth_login_path unless session[:user_id] || ENV['NONAUTH']
