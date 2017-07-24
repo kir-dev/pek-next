8f11215ce3738c265a7e896857498b88f01d3086
G <domok.gergely+github@gmail.com>
2017-07-24 17:30:16 +0200

    Add group query to search

--- app/services/search_query.rb
+++ app/services/search_query.rb
@@ -11,7 +11,21 @@ class SearchQuery
     return User.where(query, *params).order("usr_metascore DESC nulls last").offset(page.to_i * count).limit(count)
   end
 
+  def search_group(term, page)
+    page = 0 unless page
+    count = Rails.configuration.x.results_per_page
+    params = term.split.flat_map { |t| ["%#{t}%".downcase] * group_query_for_one_keyword.count('?') }
+    query = ([group_query_for_one_keyword] * term.split.size).join(' AND ')
+    return Group.where(query, *params).offset(page.to_i * count).limit(count)
+  end
+
   private
+
+  def group_query_for_one_keyword
+    <<-SQL.squish
+      (lower(grp_name) LIKE ?)
+    SQL
+  end
 
   def query_for_one_keyword
     <<-SQL.squish
