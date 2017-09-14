dbf62461263af43177a29328baf6ac7e38b2481e
kiralybalint <kiraly.balint@simonyi.bme.hu>
2017-09-14 21:27:59 +0200

    Beautify conflicting logic in groups controller and user decorator

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,12 @@
 class GroupsController < ApplicationController
   before_action :require_login
+  before_action :init, only: [:show, :edit, :update]
   before_action :require_leader, only: [:edit, :update]
+
+  def init
+    @group = Group.find(params[:id])
+    @own_membership = current_user.membership_for(@group)
+  end
 
   def index
     @groups = Group.order(:name).page(params[:page]).per(params[:per])
@@ -11,11 +17,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(update_params)
       redirect_to @group, notice: t(:edit_successful)
     else
@@ -24,7 +28,7 @@ class GroupsController < ApplicationController
   end
 
   def is_leader
-    @own_membership && @own_membership.is_leader
+    current_user.leader_of(Group.find(params[:id]))
   end
   helper_method :is_leader
 
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,17 +4,20 @@ class UserDecorator < Draper::Decorator
 
   def room
     return if user.dormitory.blank?
+    return unless Privacy.for(user, 'ROOM_NUMBER').visible
     room = [user.dormitory, user.room].join(' ')
     content_tag(:h4, room, class: 'uk-h4')
   end
 
   def cell_phone
     return if user.cell_phone.blank?
+    return unless Privacy.for(user, 'CELL_PHONE').visible
     content_tag(:h4, user.cell_phone, class: 'uk-h4')
   end
 
   def email
     return if user.email.blank?
+    return unless Privacy.for(user, 'EMAIL').visible
     content_tag(:h4, user.email, class: 'uk-h4')
   end
 
