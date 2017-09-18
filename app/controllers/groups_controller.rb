e5c4a16511e1e3242e4b1ad160d0da0202f03238
Gulyás Gergely <g.gergely314@gmail.com>
2017-09-18 14:08:51 +0200

    Check for count of delegates in a group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,18 +29,23 @@ class GroupsController < ApplicationController
 
   def delegates
     @group = Group.find(params[:group_id])
-    @eligible_members = @group.members.where(svie_member_type: 'RENDESTAG')
+    @eligible_members = @group.members.where(svie_member_type: 'RENDESTAG').order(:lastname)
       .select { |user| user.primary_membership.group_id == params[:group_id].to_i && user.primary_membership.end.nil? }
   end
 
   def set_delegate
-    # Maybe it should be in the delegate controller
+    @group = Group.find(params[:group_id])
+    delegates = @group.members.where(delegated: true).select { |user| user.primary_membership.group_id == params[:group_id].to_i && user.primary_membership.end.nil? }
+    delegate_count = delegates.length + 1
+    if delegate_count > @group.delegate_count
+      redirect_to group_delegates_path(params[:group_id]), alert: t(:too_many_delegates)
+      return
+    end
     User.find(params[:member_id]).update(delegated: true)
     redirect_to group_delegates_path(params[:group_id])
   end
 
   def delete_delegate
-    # Maybe it should be in the delegate controller
     User.find(params[:member_id]).update(delegated: false)
     redirect_to group_delegates_path(params[:group_id])
   end
