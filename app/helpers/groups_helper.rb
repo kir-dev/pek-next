a5a6cef85b82202e662053d12cf4370c045970c1
G <domok.gergely+github@gmail.com>
2017-10-14 18:41:48 +0200

    Group variable has been outsourced to parameter

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,12 +1,12 @@
 module GroupsHelper
-  def active_users
-    @viewmodel.group.memberships.where(end: nil).includes(:user).each do |membership|
+  def active_users(group)
+    group.memberships.where(end: nil).includes(:user).each do |membership|
       yield GroupMember.new(membership)
     end
   end
 
-  def inactive_users
-    @viewmodel.group.memberships.where.not(end: nil).includes(:user).each do |membership|
+  def inactive_users(group)
+    group.memberships.where.not(end: nil).includes(:user).each do |membership|
       yield GroupMember.new(membership)
     end
   end
