a5557eddc75f6852c2ebcadb173b1268ad747f3c
Gulyás Gergely <g.gergely314@gmail.com>
2017-11-26 21:25:04 +0100

    Remove worker specific configuration from Puma

--- config/puma.rb
+++ config/puma.rb
@@ -11,8 +11,8 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
+# on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+#  ActiveRecord::Base.establish_connection
+# end
