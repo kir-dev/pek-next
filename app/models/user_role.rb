dd4a0f0d1114b0b923eb31a6d1d84f935f493653
G <domok.gergely+github@gmail.com>
2017-11-30 22:37:56 +0100

    Add rvt member role to user roles

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,5 +1,6 @@
 class UserRole
   SVIE_GROUP_ID = 369
+  RVT_GROUP_ID = 146
 
   def initialize(user)
     @user = user
@@ -7,5 +8,9 @@ class UserRole
 
   def svie_admin?
     @user.groups.any? { |g| g.id == SVIE_GROUP_ID }
+  end
+
+  def rvt_member?
+    @user.groups.any? { |g| g.id == RVT_GROUP_ID }
   end
 end
