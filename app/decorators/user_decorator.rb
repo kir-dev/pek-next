101b503420b9ea9c72bb33bc75836c19fcbe5d6f
falusi94 <falusi.david94@gmail.com>
2018-01-12 00:19:08 +0100

    Add missing profile attributes

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -21,6 +21,19 @@ class UserDecorator < Draper::Decorator
     content_tag(:h4, user.email, class: 'uk-h4')
   end
 
+  def home_address
+    return if user.home_address.blank?
+    return unless Privacy.for(user, 'ADDRESS').visible ||
+      Privacy.for(user, 'HOME_ADDRESS').visible
+    content_tag(:h4, user.home_address, class: 'uk-h4')
+  end
+
+  def date_of_birth
+    return if user.date_of_birth.blank?
+    return unless Privacy.for(user, 'DATE_OF_BIRTH').visible
+    content_tag(:h4, user.date_of_birth, class: 'uk-h4')
+  end
+
   def nickname
     return if user.nickname.blank?
     user.nickname
--- app/models/privacy.rb
+++ app/models/privacy.rb
@@ -12,6 +12,7 @@ class Privacy < ActiveRecord::Base
   end
 
   def self.default_value(attribute)
-    ['WEBPAGE', 'CELL_PHONE', 'EMAIL', 'ROOM_NUMBER'].include?(attribute)
+    ['WEBPAGE', 'CELL_PHONE', 'EMAIL', 'ROOM_NUMBER', 'HOME_ADDRESS', 'ADDRESS',
+      'DATE_OF_BIRTH'].include?(attribute)
   end
 end
