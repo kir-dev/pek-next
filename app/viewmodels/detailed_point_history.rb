deceff42f9a42aa62cb8c1e7d4e91c5a1f766102
G <domok.gergely+github@gmail.com>
2018-03-15 15:58:29 +0100

    Add entry card type and explanation to DetailedPointHistory

--- app/viewmodels/detailed_point_history.rb
+++ app/viewmodels/detailed_point_history.rb
@@ -1,8 +1,9 @@
 class DetailedPointHistory
-  def initialize(pointrequest)
-    @pointrequest = pointrequest
-    @evaluation = Evaluation.find(@pointrequest.evaluation_id)
+  def initialize(point_request)
+    @point_request = point_request
+    @evaluation = Evaluation.find(@point_request.evaluation_id)
     @group = Group.find(@evaluation.group_id)
+    @entry_request = EntryRequest.find_by(usr_id: @point_request.usr_id)
   end
 
   def group_name
@@ -18,6 +19,14 @@ class DetailedPointHistory
   end
 
   def point
-    @pointrequest.point
+    @point_request.point
+  end
+
+  def entry_card_type
+    @entry_request.entry_type
+  end
+
+  def entry_card_explanation
+    @entry_request.explanation
   end
 end
