56c9f22a2784080b5816ce7356d398c987d62df3
G <domok.gergely+github@gmail.com>
2018-03-24 23:51:03 +0100

    Add type to EntryRequest and set default type

--- app/viewmodels/detailed_point_history.rb
+++ app/viewmodels/detailed_point_history.rb
@@ -3,7 +3,8 @@ class DetailedPointHistory
     @point_request = point_request
     @evaluation = Evaluation.find(@point_request.evaluation_id)
     @group = Group.find(@evaluation.group_id)
-    @entry_request = EntryRequest.find_by(usr_id: @point_request.usr_id)
+    @entry_request = EntryRequest.find_by(usr_id: @point_request.usr_id,
+                                          evaluation_id: @point_request.evaluation_id)
   end
 
   def group_name
@@ -23,10 +24,10 @@ class DetailedPointHistory
   end
 
   def entry_card_type
-    @entry_request.entry_type
+    @entry_request ? @entry_request.entry_type : EntryRequest::DEFAULT_TYPE
   end
 
   def entry_card_explanation
-    @entry_request.explanation
+    @entry_request ? @entry_request.explanation : ''
   end
 end
