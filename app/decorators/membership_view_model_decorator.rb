eed6c646cb24cf5abe651f7601edf167f54882eb
David Falusi <falusi.david94@gmail.com>
2018-04-02 12:35:10 +0200

    Fix group button width

--- app/decorators/membership_view_model_decorator.rb
+++ app/decorators/membership_view_model_decorator.rb
@@ -6,19 +6,19 @@ class MembershipViewModelDecorator < Draper::Decorator
   def edit_group_button
     return unless membership_view_model.leader?
     link_to('Adatok szerkesztése', edit_group_path(membership_view_model.group),
-      class: 'uk-button uk-button-primary uk-width-3-4')
+      class: 'uk-button uk-button-primary uk-width-1-1')
   end
 
   def edit_group_delegates_button
     return unless membership_view_model.leader? && membership_view_model.group.issvie
     link_to('Küldöttek', group_delegates_path(membership_view_model.group),
-      class: 'uk-button uk-button-primary uk-width-3-4')
+      class: 'uk-button uk-button-primary uk-width-1-1')
   end
 
   def join_group_button
     return unless membership_view_model.group.users_can_apply && !current_user.membership_for(membership_view_model.group)
     form_tag group_memberships_path(membership_view_model.group), {method: 'post'} do
-      button_tag('Jelentkezés körbe', {class: 'uk-button uk-button-primary uk-width-3-4'})
+      button_tag('Jelentkezés körbe', {class: 'uk-button uk-button-primary uk-width-1-1'})
     end
   end
 
