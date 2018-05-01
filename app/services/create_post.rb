f8ffa30cfb4c11bc93e7b542a3455a274aa469fe
David Falusi <falusi.david94@gmail.com>
2018-05-01 21:42:50 +0200

    Refactor create post
    Use proper naming conventions

--- app/services/create_post.rb
+++ app/services/create_post.rb
@@ -2,20 +2,20 @@ class CreatePost
 
   def self.call(group, membership, post_type_id)
     if post_type_id == Membership::LEADER_POST_ID
-      remove_previos_leader(group)
-      check_leader_reassign(membership)
+      remove_leader(group)
+      remove_past_leader_post(membership)
     end
-    Post.create(grp_member_id: membership.id, post_type_id: post_type_id)
+    Post.create(membership_id: membership.id, post_type_id: post_type_id)
   end
 
-  def self.remove_previos_leader(group)
-    Post.create(grp_member_id: group.leader.id,
+  def self.remove_leader(group)
+    Post.create(membership_id: group.leader.id,
       post_type_id: Membership::PAST_LEADER_ID)
     group.leader.post(Membership::LEADER_POST_ID).destroy
   end
 
-  def self.check_leader_reassign(membership)
-    Post.destroy_all(grp_member_id: membership.id,
+  def self.remove_past_leader_post(membership)
+    Post.destroy_all(membership_id: membership.id,
       post_type_id: Membership::PAST_LEADER_ID)
   end
 
