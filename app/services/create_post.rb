765a5d741711ea617a470eccd3c7569c52e5e6d1
kiralybalint <kiraly.balint@simonyi.bme.hu>
2018-05-01 08:33:05 +0200

    Fix leader post reassign

--- app/services/create_post.rb
+++ app/services/create_post.rb
@@ -1,13 +1,13 @@
 class CreatePost
 
   def self.call(group, membership, post_type_id)
-    Post.create(grp_member_id: membership.id, post_type_id: post_type_id)
     check_leader_reassign(group, post_type_id)
+    Post.create(grp_member_id: membership.id, post_type_id: post_type_id)
   end
 
   def self.check_leader_reassign(group, post_type_id)
     if post_type_id == Membership::LEADER_POST_ID
-      Post.create(grp_member_id: group.leader.id, post_type_id: Membership::PAST_LEADER_ID)
+      Post.first_or_create(grp_member_id: group.leader.id, post_type_id: Membership::PAST_LEADER_ID)
       group.leader.post(Membership::LEADER_POST_ID).destroy
       return true
     end
