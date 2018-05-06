dae9ad9f2a5dc147cc8bbd1fef7b78539782387e
kiralybalint <kiraly.balint@simonyi.bme.hu>
2018-05-06 19:42:43 +0200

    Use change instead of up and down in migration

--- db/migrate/20180501175635_add_birth_name_and_birth_place_to_users.rb
+++ db/migrate/20180501175635_add_birth_name_and_birth_place_to_users.rb
@@ -1,11 +1,6 @@
 class AddBirthNameAndBirthPlaceToUsers < ActiveRecord::Migration
-  def up
+  def change
     add_column :users, :usr_place_of_birth, :string, null: true
     add_column :users, :usr_birth_name, :string, null: true
-  end
-
-  def down
-    remove_column :users, :usr_place_of_birth
-    remove_column :users, :usr_birth_name
   end
 end
