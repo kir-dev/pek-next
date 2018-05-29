d8fb775d260b27ddfd2fb945b4e2cadaa292d5c1
salierri <g.gergely314@gmail.com>
2018-05-29 18:04:35 +0200

    Add some tests for creating an evaluation

---
+++ test/models/evaluation_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class EvaluationTest < ActiveSupport::TestCase
+
+  test 'creating a new evaluation sets default values' do
+    evaluation = Evaluation.new
+    evaluation.group = groups(:group1)
+    evaluation.semester = '201720182'
+
+    Timecop.freeze do
+      assert evaluation.save
+
+      assert evaluation.point_request_status, Evaluation::NON_EXISTENT
+      assert evaluation.entry_request_status, Evaluation::NON_EXISTENT
+      assert evaluation.justification, ''
+      assert evaluation.timestamp, Time.now
+      assert evaluation.last_modification, Time.now
+    end
+  end
+
+end
