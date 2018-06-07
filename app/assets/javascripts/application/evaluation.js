7459c500af575fe33925583614c3b64206ad25b6
David Falusi <falusi.david94@gmail.com>
2018-06-07 21:51:18 +0200

    Refactor evaluation js
    According to https://github.com/airbnb/javascript

--- app/assets/javascripts/application/evaluation.js
+++ app/assets/javascripts/application/evaluation.js
@@ -1,75 +1,62 @@
 var evaluation = (function(Rx, $) {
-    var module = {};
+  var module = {};
 
-    module.init = function() {
+  module.init = function() {
+    var input = document.querySelectorAll('.point-input');
+    if (input != null) {
+      var inputStream = Rx.Observable.fromEvent(input, 'change')
+        .map(event => event.target);
 
-        var input = document.querySelectorAll('.point-input');
-
-        if(input != null) {
-            var inputStream = Rx.Observable.fromEvent(input, "change")
-                .map(event => event.target);
-
-            inputStream.subscribe(inputChange);
-        }
-        $('.principle').click(function() {
-            principleModal($(this))
-        });
+      inputStream.subscribe(inputChange);
     }
 
-    function principleModal (principle) {
-        var sumPoint = 0, str = ``;
-        $('.point-input[data-principle="' + principle.data('id') + '"]').each(function(){
-            if($(this).val() != 0){
-                str += `<tr>
-                            <td>${$(this).data('user-name')}</td>
-                            <td>${$(this).val()}</td>
-                        </tr>`;
-                sumPoint += parseInt($(this).val());
-            }
-        });
-        UIkit.modal.alert(
-            `<h2>${principle.text()}</h2>
-             <p>${principle.data('description')}</p>
-             <table class="uk-table uk-table-striped">
-                ${str}
-             <tr>
-                    <td>
-                        <strong>Összeg</strong>
-                    </td>
-                    <td>
-                        <strong>${sumPoint}</strong>
-                    </td>
-                </tr>
-            </table>`
-        );
-    }
+    $('.principle').click(function() {
+      principleModal($(this));
+    });
+  }
 
-    function inputChange (input) {
-        $('#save-icon').fadeIn();
-        var value = input.value;
-        var principle = input.getAttribute('data-principle');
-        var user = input.getAttribute('data-user');
-        $.ajax({
-            method: 'POST',
-            url: updateURL,
-            data: {
-                principle_id: principle,
-                user_id: user,
-                point: value
-            }
-        }).done(function (resp) {
-            $('#save-icon').fadeOut();
-        }).error(function () {
-            UIkit.notify({
-                message: "Hiba történt a mentés során! Ellenőrizd az internetkapcsolatod!",
-                timeout: 10000,
-                status: 'danger'
-            });
-            $('#save-icon').fadeOut();
-        });
-    }
+  function principleModal(principle) {
+    var sumPoint = 0, table = '';
 
-    return module;
+    $('.point-input[data-principle="' + principle.data('id') + '"]').each(function(){
+      var value = $(this).val();
+      if (value == 0) { return; }
+
+      var userName = $(this).data('user-name');
+      table += '<tr><td>' + userName + '</td><td>' + value + '</td></tr>';
+      sumPoint += parseInt(value);
+    });
+
+    UIkit.modal.alert('<h2>' + principle.text() + '</h2><p>' + principle.data('description') + '</p><table class="uk-table uk-table-striped">' + table + '<tr><td><strong>Összeg</strong></td><td><strong>' + sumPoint + '</strong></td></tr></table>');
+  }
+
+  function inputChange(input) {
+    $('#save-icon').fadeIn();
+    var value = input.value;
+    var principle = input.getAttribute('data-principle');
+    var user = input.getAttribute('data-user');
+
+    $.ajax({
+      method: 'POST',
+      url: updateURL,
+      data: {
+        principle_id: principle,
+        user_id: user,
+        point: value
+      }
+    }).done(function (resp) {
+      $('#save-icon').fadeOut();
+    }).error(function () {
+      UIkit.notify({
+        message: 'Hiba történt a mentés során! Ellenőrizd az internetkapcsolatod!',
+        timeout: 10000,
+        status: 'danger'
+      });
+      $('#save-icon').fadeOut();
+    });
+  }
+
+  return module;
 }(Rx, jQuery));
 
 $(document).ready(evaluation.init);
