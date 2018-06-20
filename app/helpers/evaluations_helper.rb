e66d40564b15e09e2c44d3a0bf51bbd6a52637e2
David Falusi <falusi.david94@gmail.com>
2018-06-21 00:18:59 +0200

    Add 20/30 points rule

--- app/helpers/evaluations_helper.rb
+++ app/helpers/evaluations_helper.rb
@@ -3,16 +3,25 @@ module EvaluationsHelper
     point_detail = point_details.find { |pd| pd.point_request.user == user && pd.principle_id == principle.id }&.point
   end
 
-  def sum_details(point_details, user, principle_type)
-    point_details.select { |pd| pd.point_request.user == user && pd.principle.type == principle_type }
-    .sum { |pd| pd.point }
+  def sum_work_details(point_details, user)
+    [sum_details(point_details, user, Principle::WORK), 30].min
+  end
+
+  def sum_responsibility_details(point_details, user)
+    [sum_details(point_details, user, Principle::RESPONSIBILITY), 20].min
   end
 
   def sum_all_details(point_details, user)
-    point_details.select { |pd| pd.point_request.user == user }.sum { |pd| pd.point }
+    sum_responsibility_details(point_details, user) + sum_work_details(point_details, user)
   end
 
   def entry_request(evaluation, user)
     evaluation.entry_requests.find { |er| er.user == user }&.entry_type || EntryRequest::DEFAULT_TYPE
   end
+
+  private
+    def sum_details(point_details, user, principle_type)
+      point_details.select { |pd| pd.point_request.user == user && pd.principle.type == principle_type }
+      .sum { |pd| pd.point }
+    end
 end
