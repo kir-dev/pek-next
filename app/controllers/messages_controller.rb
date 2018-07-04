ef00b8435ee4eb70af7208c48274e64344cffd02
David Falusi <falusi.david94@gmail.com>
2018-07-05 00:31:18 +0200

    Add messaging for group leader

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,34 @@
+class MessagesController < ApplicationController
+  before_action :require_login
+  before_action :require_leader
+  before_action :require_application_or_evaluation_season
+
+  def index
+    @semester = SystemAttribute.semester.to_s
+    @group = Group.find(params[:group_id])
+    @evaluation_messages =
+      EvaluationMessage.where(group: @group, semester: @semester)
+                       .order(sent_at: :desc).page(params[:page]).decorate
+  end
+
+  def all
+    @group = Group.find(params[:group_id])
+    @evaluation_messages =
+      EvaluationMessage.where(group: @group)
+                       .order(sent_at: :desc).page(params[:page]).decorate
+    render :index
+  end
+
+  def create
+    semester = SystemAttribute.semester.to_s
+    group = Group.find(params[:group_id])
+    EvaluationMessage.create(message: params[:message], group: group, sender_user: current_user, sent_at: DateTime.now, semester: semester)
+    redirect_back fallback_location: group_messages_path(current_group)
+  end
+
+  private
+
+  def require_application_or_evaluation_season
+    redirect_to root_url if SystemAttribute.offseason?
+  end
+end
