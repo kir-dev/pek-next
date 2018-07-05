9b83903a27e241e8d2b70622c235a6f269ecf58a
David Falusi <falusi.david94@gmail.com>
2018-07-05 18:19:52 +0200

    Add auto selection to profile settings switcher tab

--- app/assets/javascripts/application/helpers.js
+++ app/assets/javascripts/application/helpers.js
@@ -16,5 +16,31 @@ var Helpers = (function () {
     }
   };
 
+  helpers.initSwitcher = function(switcherId) {
+    window.setTimeout(function() {
+      openTab(switcherId);
+    }, 50);
+    initLinks(switcherId);
+  }
+
+  function openTab(switcherId) {
+    var hash = window.location.hash;
+    if (!hash) { return }
+
+    var link = $('#' + switcherId + ' a[href="' + hash + '"]');
+    link.click();
+  }
+
+  function initLinks(switcherId) {
+    $('#' + switcherId + ' a').click(function() {
+      var val = $(this).attr('href');
+      window.location.hash = val.replace("#", "");
+      links = $('.uk-pagination a');
+      links.each(function(id) {
+        links[id].href = links[id].href.split('#')[0] + val;
+      });
+    });
+  }
+
   return helpers;
 }());
