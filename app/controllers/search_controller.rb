06d495958f06d37d2951e96fca48111d34f1b161
David Falusi <falusi.david94@gmail.com>
2018-07-23 22:57:05 +0200

    Update search listing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,6 +9,7 @@ class SearchController < ApplicationController
 
     if params.key?(:query)
       group_results = SearchQuery.new.group_search(params[:query], params[:group_count])
+      group_results = group_results.decorate
       count = Rails.configuration.x.results_per_page - group_results.count
       user_results = SearchQuery.new.user_search(params[:query], params[:user_count], count)
       user_results = user_results.decorate
