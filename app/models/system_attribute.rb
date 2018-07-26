13546aea9cd244e346bc1b89ae4a4e12717f4296
David Falusi <falusi.david94@gmail.com>
2018-07-26 03:21:05 +0200

    Add point history calculation

--- app/models/system_attribute.rb
+++ app/models/system_attribute.rb
@@ -1,15 +1,15 @@
 class SystemAttribute < ActiveRecord::Base
-  self.table_name = "system_attrs"
+  self.table_name = 'system_attrs'
   self.primary_key = :attributeid
   alias_attribute :value, :attributevalue
   alias_attribute :name, :attributename
 
   def self.semester
-    Semester.new(find_by(name: "szemeszter").value)
+    Semester.new(find_by(name: 'szemeszter').value)
   end
 
   def self.update_semester(semester)
-    find_by(name: "szemeszter").update(value: semester)
+    find_by(name: 'szemeszter').update(value: semester)
   end
 
   def self.season
@@ -19,11 +19,15 @@ class SystemAttribute < ActiveRecord::Base
   def self.update_season(season)
     self.season.update(value: season)
     EntryRequest.remove_justifications unless application_season?
+    return unless offseason?
+    semester = SystemAttribute.semester
+    calculate_point_history = CalculatePointHistory.new(semester)
+    calculate_point_history.call
   end
 
-  OFFSEASON = 'NINCSERTEKELES'
-  APPLICATION_SEASON = 'ERTEKELESLEADAS'
-  EVALUATION_SEASON = 'ERTEKELESELBIRALAS'
+  OFFSEASON = 'NINCSERTEKELES'.freeze
+  APPLICATION_SEASON = 'ERTEKELESLEADAS'.freeze
+  EVALUATION_SEASON = 'ERTEKELESELBIRALAS'.freeze
 
   def self.application_season?
     season.value == APPLICATION_SEASON
