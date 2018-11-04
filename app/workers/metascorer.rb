b076c8f9abfbd0678990c1480179a2c227fc143f
G <8500183+emanreyalp@users.noreply.github.com>
2018-11-05 00:35:00 +0100

    Favor modifier if/unless usage when you have a single-line body

--- app/workers/metascorer.rb
+++ app/workers/metascorer.rb
@@ -18,35 +18,21 @@ class Metascorer
     metascore = 0;
     last_sem_point = PointHistory.find_by(usr_id: user.id, semester: SystemAttribute.semester.previous.to_s)
     last_year_point = PointHistory.find_by(usr_id: user.id, semester: SystemAttribute.semester.previous.previous.to_s)
-    if(last_sem_point)
-      metascore += last_sem_point.point * @config[:last_semester_multiplier]
-    end
-    if(last_year_point)
-      metascore += last_year_point.point * @config[:last_year_multiplier]
-    end
-    if user.photo_path.present?
-      metascore += @config[:photo_reward]
-    end
-    if user.last_login.present?
-      metascore += last_login_reward(user.last_login)
-    end
-    if user.cell_phone.present?
-      metascore += @config[:phone_number_reward]
-    end
-    if user.dormitory.present?
-      metascore += @config[:dormitory_reward]
-    end
-    if user.email.present?
-      metascore += @config[:email_reward]
-    end
+
+    metascore += last_sem_point.point * @config[:last_semester_multiplier] if last_sem_point
+    metascore += last_year_point.point * @config[:last_year_multiplier] if last_year_point
+    metascore += @config[:photo_reward] if user.photo_path.present?
+    metascore += last_login_reward(user.last_login) if user.last_login.present?
+    metascore += @config[:phone_number_reward] if user.cell_phone.present?
+    metascore += @config[:dormitory_reward] if user.dormitory.present?
+    metascore += @config[:email_reward] if user.email.present?
+
     return metascore
   end
 
   def last_login_reward(last_login)
     @config[:login_rewards].each do |tier|
-      if last_login > Date.today - tier[:time]
-        return tier[:reward]
-      end
+      return tier[:reward] if last_login > Date.today - tier[:time]
     end
     return 0
   end
