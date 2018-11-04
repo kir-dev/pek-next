134dad5f24c098551609d61a1522163dc7609b6f
G <8500183+emanreyalp@users.noreply.github.com>
2018-11-05 00:40:46 +0100

    Reorder metascore's additions according to alphabets
    Maybe we will not add the same config twice.

--- app/workers/metascorer.rb
+++ app/workers/metascorer.rb
@@ -15,17 +15,18 @@ class Metascorer
   end
 
   def calculate_meta_score(user)
-    metascore = 0;
     last_sem_point = PointHistory.find_by(usr_id: user.id, semester: SystemAttribute.semester.previous.to_s)
     last_year_point = PointHistory.find_by(usr_id: user.id, semester: SystemAttribute.semester.previous.previous.to_s)
 
+    metascore = 0
+
     metascore += last_sem_point.point * @config[:last_semester_multiplier] if last_sem_point
     metascore += last_year_point.point * @config[:last_year_multiplier] if last_year_point
-    metascore += @config[:photo_reward] if user.photo_path.present?
-    metascore += last_login_reward(user.last_login) if user.last_login.present?
-    metascore += @config[:phone_number_reward] if user.cell_phone.present?
     metascore += @config[:dormitory_reward] if user.dormitory.present?
     metascore += @config[:email_reward] if user.email.present?
+    metascore += @config[:phone_number_reward] if user.cell_phone.present?
+    metascore += @config[:photo_reward] if user.photo_path.present?
+    metascore += last_login_reward(user.last_login) if user.last_login.present?
 
     return metascore
   end
