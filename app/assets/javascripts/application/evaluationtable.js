334792ba6f35b7ddcf956be348e84c79a6769727
David Falusi <falusi.david94@gmail.com>
2018-12-03 21:11:06 +0100

    Save hidden users on evaluation page to local storage

--- app/assets/javascripts/application/evaluationtable.js
+++ app/assets/javascripts/application/evaluationtable.js
@@ -10,20 +10,27 @@ var evaluationTable = (function($) {
     hiddenUsersList = $('#hidden-users-list');
     hiddenUsersButton = $('#hidden-users-button');
     table.arrowTable();
+    hideUsers();
   }
 
   module.showRow = function(userId) {
     const button = $('.hidden-user*[data-id=' + userId + ']');
     button.remove();
     changeRowVisibility(userId, true);
+    removeFromHiddenList(userId);
     updateHiddenUserButtonState();
   }
 
   function hideRow() {
     const userId = this.getAttribute('data-id');
+    hideRowById(userId);
+  }
+
+  function hideRowById(userId) {
     const name = $('.name-list-item*[data-id=' + userId + ']').text();
     hiddenUsersList.append('<li class="hidden-user" data-id="' + userId + '"><a href="#" onclick="evaluationTable.showRow(' + userId + ')" title="mutatás" uk-tooltip>' + name + '<i class="uk-icon-eye"></i></a></li>');
     changeRowVisibility(userId, false);
+    addToHiddenList(userId);
     updateHiddenUserButtonState();
   }
 
@@ -45,6 +52,38 @@ var evaluationTable = (function($) {
     } else {
       hiddenUsersButton.addClass('uk-button-danger');
     }
+  }
+
+  function hideUsers() {
+    usersList = getHiddenListFromStorage();
+    localStorage.removeItem('hiddenUsers');
+    for (var i=0, len=usersList.length; i<len; i++) {
+      hideRowById(usersList[i]);
+    }
+  }
+
+  function removeFromHiddenList(userId) {
+    let newList = getHiddenListFromStorage();
+    const index = newList.indexOf(userId.toString());
+    if (index<0) { return; }
+    newList.splice(index, 1);
+    saveHiddenListToStorage(newList);
+  }
+
+  function addToHiddenList(userId) {
+    const newList = getHiddenListFromStorage();
+    newList.push(userId);
+    saveHiddenListToStorage(newList);
+  }
+
+  function getHiddenListFromStorage() {
+    const list = localStorage.getItem('hiddenUsers');
+    if (!list) { return []; }
+    return JSON.parse(list);
+  }
+
+  function saveHiddenListToStorage(list) {
+    localStorage.setItem('hiddenUsers', JSON.stringify(list));
   }
 
   return module;
