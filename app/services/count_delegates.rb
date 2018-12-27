fe37e55cbb116a17803b0816177505eafbb9bb7f
David Falusi <falusi.david94@gmail.com>
2018-12-28 00:23:52 +0100

    Update delegate counting

--- app/services/count_delegates.rb
+++ app/services/count_delegates.rb
@@ -1,11 +1,4 @@
 class CountDelegates
-  def initialize
-    @primary_users =
-      User.primary_svie_members
-          .includes(primary_membership: [:posts, :post_types, { user: [:primary_membership] }])
-          .select { |user| user.primary_membership&.primary? }
-  end
-
   def call
     reset_delegate_count
     count_delegates
@@ -19,15 +12,13 @@ class CountDelegates
 
   def count_delegates
     group_member_count = Hash.new 0
-    @primary_users.map { |user| user.primary_membership.group_id }
-                  .each { |group_id| group_member_count[group_id] += 1 }
-    group_member_count.each { |group_id, member_count| set_delegate_count(group_id, member_count) }
+    primary_users.map { |user| user.primary_membership.group }
+                 .each { |group| group_member_count[group] += 1 }
+    group_member_count.each { |group, member_count| set_delegate_count(group, member_count) }
   end
 
-  def set_delegate_count(group_id, member_count)
-    return if member_count < 5
-
-    group = Group.find(group_id)
+  def set_delegate_count(group, member_count)
+    return 0 if member_count < 5
     return unless group.issvie
 
     if member_count.between? 5, 20
@@ -35,5 +26,12 @@ class CountDelegates
     else
       group.update delegate_count: member_count / 20
     end
+  end
+
+  def primary_users
+    User.primary_svie_members
+        .includes(primary_membership: [:posts, :post_types, :group,
+                                       { user: [:primary_membership] }])
+        .select { |user| user.primary_membership&.primary? }
   end
 end
