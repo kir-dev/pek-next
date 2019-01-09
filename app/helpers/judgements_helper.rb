dea07c1b6dc1f800f63444b22f869a5e9f6289b5
David Falusi <falusi.david94@gmail.com>
2019-01-10 00:04:10 +0100

    Add work/responsibility/all points average on judgement page

--- app/helpers/judgements_helper.rb
+++ app/helpers/judgements_helper.rb
@@ -11,6 +11,18 @@ module JudgementsHelper
     points(point_details, memberships, method(:sum_all_details)).sum
   end
 
+  def work_points_average(point_details, memberships)
+    average(points(point_details, memberships, method(:sum_work_details)))
+  end
+
+  def responsibility_points_average(point_details, memberships)
+    average(points(point_details, memberships, method(:sum_responsibility_details)))
+  end
+
+  def all_points_average(point_details, memberships)
+    average(points(point_details, memberships, method(:sum_all_details)))
+  end
+
   private
 
   def points(point_details, memberships, method)
@@ -20,5 +32,12 @@ module JudgementsHelper
       points.push sum if sum.positive?
     end
     points
+  end
+
+  def average(points_array)
+    return 0 if points_array.empty?
+
+    average = points_array.sum / points_array.size.to_f
+    average.round(2)
   end
 end
