51ddc7ce3dd99c292a38ef5d5a49e01cf37d0642
David Falusi <falusi.david94@gmail.com>
2019-02-19 15:54:38 +0100

    Fix failing Travis

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,4 +1,6 @@
 Unsplash.configure do |config|
+  return if Rails.env.test?
+
   config.application_access_key = ENV.fetch('UNSPLASH_ACCES_KEY')
   config.application_secret = ENV.fetch('UNSPLASH_SECRET')
   config.utm_source = 'pek_error_images'
