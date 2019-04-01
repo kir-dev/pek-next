454a5687a55abea395812d6d57ee3472d8c4d369
Gergely Dömök <emanreyalp@users.noreply.github.com>
2019-04-01 08:37:03 +0200

    Change fixtures to factories in group model test

--- test/factories/groups.rb
+++ test/factories/groups.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :group do
     name { 'Babhámozó' }
     type { 'szakmai kör' }
+
+    trait :with_additional_info do
+      description { 'vietnámiak' }
+      webpage { 'http://babhamozo.sch.bme.hu' }
+      maillist { 'babhamozo@sch.bme.hu' }
+      founded { 1998 }
+    end
   end
 end
