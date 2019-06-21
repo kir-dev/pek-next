5230c15f5d9df4f8cfb0d92ee87efbbea376214c
David Falusi <falusi.david94@gmail.com>
2019-06-21 18:18:41 +0200

    Add redirect to group history when there is not any evaluation

--- app/controllers/group_history_controller.rb
+++ app/controllers/group_history_controller.rb
@@ -1,4 +1,6 @@
 class GroupHistoryController < ApplicationController
+  before_action :redirect_back_with_notice, unless: :has_evaluation_to_show?
+
   def show
     @group = current_group
     @evaluations = @group.accepted_evaluations_by_date
@@ -7,5 +9,15 @@ class GroupHistoryController < ApplicationController
     end
     @evaluation ||= @evaluations.first
     @semester = @evaluation.date_as_semester
+  end
+
+  private
+
+  def has_evaluation_to_show?
+    current_group.accepted_evaluations_by_date.any?
+  end
+
+  def redirect_back_with_notice
+    redirect_back fallback_location: group_path(current_group), alert: t(:does_not_have_evaluation)
   end
 end
