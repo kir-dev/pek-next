1df2b4e53c043baec5a576c345a50ae487f5834d
János Fehér <feherjani96@gmail.com>
2020-03-13 15:37:47 +0100

    Migrate profiles controller tests to rspec #175

---
+++ spec/requests/profiles_controller_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+describe ProfilesController do
+  let(:user)        { create(:user) }
+  let!(:other_user) { create(:user) }
+  before { login_as(user) }
+
+  describe '#show_self' do
+    it 'shows the own profile of the user' do
+      get "/profiles/#{user.screen_name}"
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    context 'when the screenname is invalid' do
+      it 'redirects to own profile' do
+        get '/profiles/dummy'
+
+        expect(response).to redirect_to(profiles_me_path)
+      end
+    end
+  end
+
+  describe '#show' do
+    it 'shows the profile of the user' do
+      get "/profiles/#{other_user.screen_name}"
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe '#edit' do
+    it 'renders the page' do
+      get "/profiles/#{user.screen_name}/edit"
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe '#update' do
+    it 'updates the user' do
+      patch "/profiles/#{user.screen_name}", profile: { firstname: 'Happy' }
+
+      expect(user.reload.firstname).to eq 'Happy'
+    end
+
+    it 'redirects to profile path' do
+      patch "/profiles/#{user.screen_name}", profile: { firstname: 'Happy' }
+
+      expect(response).to redirect_to(profiles_me_path)
+    end
+  end
+end
