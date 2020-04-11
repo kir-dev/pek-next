b6642aefebfb50d9a919c5b10e17e50931476443
János Fehér <feherjani96@gmail.com>
2020-04-11 02:02:24 +0200

    Move EvaluationControllerTest to rspec #175

---
+++ spec/requests/evalutations_controller_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+describe EvaluationsController do
+  describe '#current' do
+    let(:group) { create(:group) }
+
+    context 'when the user is not the group leader' do
+      it 'returns forbidden' do
+        user = create(:user)
+        login_as(user)
+
+        get "/groups/#{group.id}/evaluations/current"
+
+        expect(response).to have_http_status :forbidden
+      end
+    end
+
+    context 'when the user is the group leader' do
+      let(:user)    { group.leader.user }
+      before(:each) { login_as(user) }
+
+      it 'creates new evaluation' do
+        get "/groups/#{group.id}/evaluations/current"
+
+        new_evaluation = Evaluation.find_by(
+          group: group, semester: SystemAttribute.semester.to_s
+        )
+
+        expect(new_evaluation).not_to be nil
+      end
+
+      it 'redirects to the evaluation page' do
+        get "/groups/#{group.id}/evaluations/current"
+
+        new_evaluation = Evaluation.find_by(
+          group: group, semester: SystemAttribute.semester.to_s
+        )
+
+        expect(response).to redirect_to group_evaluation_path(group, new_evaluation)
+      end
+    end
+  end
+end
