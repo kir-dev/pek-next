c88dea68cf76b963ac7b9b1f05da17dd77511127
David Falusi <falusi.david94@gmail.com>
2020-04-12 10:31:55 +0200

    Refactor MembershipsController

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,11 +1,10 @@
 class MembershipsController < ApplicationController
-  before_action :require_leader, only: %i[inactivate destroy reactivate archive accept unarchive]
+  before_action :require_leader, except: :create
 
   def create
-    @group = Group.find(params[:group_id])
     Membership::CreateService.call(group, current_user)
 
-    redirect_back(fallback_location: group_path(@group))
+    redirect_back(fallback_location: group_path(group))
   rescue Membership::CreateService::AlreadyMember, Membership::CreateService::GroupNotReceivingNewMembers
     forbidden_page
   end
@@ -37,5 +36,11 @@ class MembershipsController < ApplicationController
 
   def accept
     Membership.find(params[:membership_id]).accept!
+  end
+
+  private
+
+  def group
+    @group ||= Group.find(params[:group_id])
   end
 end
