7e8c7f017f87c67a1bdb624c837c5148b9568f1c
János Fehér <feherjani96@gmail.com>
2020-04-17 23:41:11 +0200

    Update config to generate https links #177

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  routes.default_url_options.[:host] = 'pek.sch.bme.hu'
+
+  config.ssl_options = { redirect: { host: 'pek.sch.bme.hu' } }
 end
