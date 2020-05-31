df72877eff4d687a539e585e145637ad3ca9467c
David Falusi <falusi.david94@gmail.com>
2020-05-31 13:05:02 +0200

    Remove deprecated raise_in_transactional_callbacks flag
    https://github.com/rails/rails/commit/07d3d402341e81ada0214f2cb2be1da69eadfe72

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,6 @@ module PekNext
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :hu
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.active_record.schema_format = :sql
 
     config.x.photo_path = 'public/uploads/'
