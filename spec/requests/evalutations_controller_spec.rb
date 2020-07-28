07a4b9919fbc0d4c49aab8cc18caf7dc3395fc4f
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-07-28 14:14:52 +0200

    Add tests for evaluation

--- spec/requests/evalutations_controller_spec.rb
+++ spec/requests/evalutations_controller_spec.rb
@@ -38,6 +38,31 @@ describe EvaluationsController do
 
         expect(response).to redirect_to group_evaluation_path(group, new_evaluation)
       end
+
+      it 'test a lot' do
+        SystemAttribute.update_season(SystemAttribute::EVALUATION_SEASON)
+        point_detail = create(:point_detail)
+        evaluation = point_detail.point_request.evaluation
+        evaluation.update(point_request_status: Evaluation::REJECTED)
+        group = evaluation.group
+        request_params = {principle_id: point_detail.principle_id, user_id: point_detail.point_request.user_id, point: 3}
+        login_as(group.leader.user)
+
+        number = 1000
+        count = 0
+        number.times do
+          expect do
+            puts "#{count += 1}/#{number}"
+            evaluation.point_requests.each { |pr| pr.point_details.destroy_all }
+            post "/groups/#{group.id}/evaluations/#{evaluation.id}/pointdetails/update", {params: request_params, xhr: true}
+            expect(response).to have_http_status(:ok)
+
+            post "/groups/#{group.id}/evaluations/#{evaluation.id}/pointdetails/update", {params: request_params, xhr: true}
+            expect(response).to have_http_status(:ok)
+
+          end.to change { PointDetail.count }.by(0)
+        end
+      end
     end
   end
 end
