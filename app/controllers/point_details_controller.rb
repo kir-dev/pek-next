55edafe9a9c414cf6d490d954ec44307c9075170
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-08-29 20:49:16 +0200

    Add validation for point_detail group

--- app/controllers/point_details_controller.rb
+++ app/controllers/point_details_controller.rb
@@ -2,6 +2,7 @@ class PointDetailsController < ApplicationController
   before_action :require_resort_or_group_leader
   before_action :set_entities
   before_action :changeable_evaluation
+  before_action :validate_correct_group
 
   def update
     point_detail_service = CreateOrUpdatePointDetail.new(@user, @principle, @evaluation)
@@ -18,5 +19,9 @@ class PointDetailsController < ApplicationController
 
   def changeable_evaluation
     redirect_to root_url unless @evaluation.changeable_point_request_status?
+  end
+
+  def validate_correct_group
+    forbidden_page unless current_evaluation.group == current_group
   end
 end
