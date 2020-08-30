518d7fc02315de59ee79957fca328e629f93d307
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-08-30 21:17:49 +0200

    Refactor PointDetail service

--- app/services/create_or_update_point_detail.rb
+++ app/services/create_or_update_point_detail.rb
@@ -9,10 +9,10 @@ class CreateOrUpdatePointDetail
 
   def call(point)
     ActiveRecord::Base.transaction do
-      point_request = PointRequest.find_or_create_by(evaluation_id: evaluation.id, user_id: user.id)
-      point_detail = PointDetail.find_or_create_by(point_request_id: point_request.id,
+      point_request = PointRequest.find_or_create_by!(evaluation_id: evaluation.id, user_id: user.id)
+      point_detail = PointDetail.find_or_create_by!(point_request_id: point_request.id,
                                                    principle_id: principle.id)
-      point_detail.update(point: point)
+      point_detail.update!(point: point)
 
       point_details = PointDetail.includes(%i[point_request principle]).select do |pd|
         pd.point_request.evaluation_id == evaluation.id && pd.point_request.user_id == user.id
