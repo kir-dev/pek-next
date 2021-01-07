a3fec798715ce98f9d255a899cfff65f49dd2530
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-01-07 22:37:18 +0100

    move retry to controller

--- app/services/create_or_update_point_detail.rb
+++ app/services/create_or_update_point_detail.rb
@@ -9,19 +9,10 @@ class CreateOrUpdatePointDetail
 
   def call(point)
     ActiveRecord::Base.transaction do
-      begin
-        point_request = PointRequest.find_or_create_by!(evaluation_id: evaluation.id, user_id: user.id)
-      rescue ActiveRecord::RecordNotUnique
-        retry
-      end
+      point_request = PointRequest.find_or_create_by!(evaluation_id: evaluation.id, user_id: user.id)
 
-      begin
-        point_detail = PointDetail.find_or_create_by!(point_request_id: point_request.id,
-                                                      principle_id: principle.id)
-      rescue ActiveRecord::RecordNotUnique
-        retry
-      end
-
+      point_detail = PointDetail.find_or_create_by!(point_request_id: point_request.id,
+                                                    principle_id: principle.id)
       point_detail.update!(point: point)
 
       point_details = PointDetail.includes(%i[point_request principle]).select do |pd|
