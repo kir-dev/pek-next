080a1586708909c83cab162a9bb39d232e7c43ed
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-01-07 20:47:12 +0100

    add retry to create or update point detail

--- app/services/create_or_update_point_detail.rb
+++ app/services/create_or_update_point_detail.rb
@@ -9,7 +9,14 @@ class CreateOrUpdatePointDetail
 
   def call(point)
     ActiveRecord::Base.transaction do
-      point_request = PointRequest.find_or_create_by!(evaluation_id: evaluation.id, user_id: user.id)
+      try = 0
+      begin
+        try += 1
+        point_request = PointRequest.find_or_create_by!(evaluation_id: evaluation.id, user_id: user.id)
+      rescue ActiveRecord::RecordNotUnique
+        retry unless try >= 20
+      end
+
       point_detail = PointDetail.find_or_create_by!(point_request_id: point_request.id,
                                                    principle_id: principle.id)
       point_detail.update!(point: point)
