9ee389a19ec1bc56f4c58e2baac3aae797512340
balintkiraly <balint.kiraly@protonmail.com>
2021-02-07 16:04:22 +0100

    Add only_active flag to groups fetch

--- app/graphql/queries/fetch_groups.rb
+++ app/graphql/queries/fetch_groups.rb
@@ -1,9 +1,11 @@
 module Queries
   class FetchGroups < Queries::BaseQuery
+    argument :only_active, Boolean, required: false
     type Types::GroupType.connection_type, null: false
 
-    def resolve
-      Group.all
+    def resolve(only_active: nil)
+      return Group.order(:name).reject(&:inactive?) if only_active
+      Group.order(:name)
     end
   end
 end
