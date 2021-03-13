c92baddac7b6840de149e81d83e695f1431eb74a
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-03-13 10:41:14 -0800

    update application to Rails 6.0

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,17 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
 # Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( *.css *.svg *.eot *.woff *.ttf )
