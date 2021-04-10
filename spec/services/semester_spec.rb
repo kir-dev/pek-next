e240784114de65cafb029c3a481cef968a5e3d75
Oravecz Márton Péter <oravecz.marton@gmail.com>
2021-04-10 04:09:18 -0700

    Semester check #283 and #180 Fixing tests valid but incorrect declaration of semesters.

--- spec/services/semester_spec.rb
+++ spec/services/semester_spec.rb
@@ -1,42 +1,42 @@
 describe Semester do
   it "Checking autumn semester calculated current in semester's January" do
     Timecop.freeze '2020-01-01' do
-      expect(Semester.new("2019202001").current?).to be true
+      expect(Semester.new("201920201").current?).to be true
     end
   end
   it "Checking autumn semester calculated current in semester's start (September 1st)" do
     Timecop.freeze '2019-09-01' do
-      expect(Semester.new("2019202001").current?).to be true
+      expect(Semester.new("201920201").current?).to be true
     end
   end
   it "Checking autumn semester calculated current in emd of year in semester" do
     Timecop.freeze '2019-12-31' do
-      expect(Semester.new("2019202001").current?).to be true
+      expect(Semester.new("201920201").current?).to be true
     end
   end
   it "Checking autumn semester calculated not current  start of next semester (February 1st)" do
     Timecop.freeze '2020-02-01' do
-      expect(Semester.new("2019202001").current?).to be false
+      expect(Semester.new("201920201").current?).to be false
     end
   end
   it "Checking autumn semester calculated not current in previous year's semester" do
     Timecop.freeze '2019-01-01' do
-      expect(Semester.new("2019202001").current?).to be false
+      expect(Semester.new("201920201").current?).to be false
     end
   end
   it "Checking autumn semester calculated not current in next year's semester" do
     Timecop.freeze '2021-01-01' do
-      expect(Semester.new("2019202001").current?).to be false
+      expect(Semester.new("201920201").current?).to be false
     end
   end
   it "Checking autumn semester calculated not current in previous semester (August 31th)" do
     Timecop.freeze '2019-08-31' do
-      expect(Semester.new("2019202001").current?).to be false
+      expect(Semester.new("201920201").current?).to be false
     end
   end
   it "Checking autumn semester calculated not current in next semester's January" do
     Timecop.freeze '2020-02-01' do
-      expect(Semester.new("2019202001").current?).to be false
+      expect(Semester.new("201920201").current?).to be false
     end
   end
 
@@ -47,32 +47,32 @@ describe Semester do
   end
   it "Checking spring semester calculated current in semester's start (February 1st)" do
     Timecop.freeze '2020-02-01' do
-      expect(Semester.new("2019202002").current?).to be true
+      expect(Semester.new("201920202").current?).to be true
     end
   end
   it "Checking spring semester calculated current in emd of semester (August 31st)" do
     Timecop.freeze '2020-08-31' do
-      expect(Semester.new("2019202002").current?).to be true
+      expect(Semester.new("201920202").current?).to be true
     end
   end
   it "Checking spring semester calculated not current start of next semester (September 1st)" do
     Timecop.freeze '2020-09-01' do
-      expect(Semester.new("2019202001").current?).to be false
+      expect(Semester.new("201920201").current?).to be false
     end
   end
   it "Checking spring semester calculated not current in previous years's semester" do
     Timecop.freeze '2019-02-01' do
-      expect(Semester.new("2019202001").current?).to be false
+      expect(Semester.new("201920201").current?).to be false
     end
   end
   it "Checking autumn semester calculated not current in previous semester (August 31th)" do
     Timecop.freeze '2019-08-31' do
-      expect(Semester.new("2019202002").current?).to be false
+      expect(Semester.new("201920202").current?).to be false
     end
   end
   it "Checking autumn semester calculated not current in next semester's January" do
     Timecop.freeze '2021-02-01' do
-      expect(Semester.new("2019202002").current?).to be false
+      expect(Semester.new("201920202").current?).to be false
     end
   end
 end
