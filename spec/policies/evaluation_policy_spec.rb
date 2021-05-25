b70a0d9aee6ee48d54c3cd363f5643d1dc3e8f4d
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-05-25 12:02:32 -0700

    add test for update comment in EvaluationPolicy

--- spec/policies/evaluation_policy_spec.rb
+++ spec/policies/evaluation_policy_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe EvaluationPolicy, type: :policy do
   let(:submit_actions)          { %i[submit_point_request submit_entry_request] }
   let(:cancel_actions)          { %i[cancel_point_request cancel_entry_request] }
   let(:update_request_actions)  { %i[update_point_request update_entry_request] }
+  let(:update_comment_actions)  { %i[update_comments] + evaluation_view_actions }
   let(:all_action)              { entry_request_actions + point_request_actions + evaluation_actions }
 
   context 'when application season' do
@@ -18,6 +19,7 @@ RSpec.describe EvaluationPolicy, type: :policy do
       let(:user) { evaluation.group.leader.user }
 
       it { is_expected.to permit_actions(all_action - cancel_actions) }
+      it { is_expected.to permit_actions(update_comment_actions) }
       it { is_expected.to forbid_actions(cancel_actions) }
 
       context "and the request is submitted" do
