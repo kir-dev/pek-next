34d831a2307b6b35d618bb8eb66662a2ff9b6dc7
Sepsi László <48379676+SepsiLaszlo@users.noreply.github.com>
2021-11-29 19:20:51 +0100

    Improve post mgmt (#372)
     add group_posts page
    
    Co-Authored-By: Sámuel Fekete <13004605+Tschonti@users.noreply.github.com>
    Co-Authored-By: DannySS123 <65909866+DannySS123@users.noreply.github.com>
    Co-Authored-By: Beni <25415958+poiuztrewq1@users.noreply.github.com>
    Co-Authored-By: Bálint Berente <30603208+berenteb@users.noreply.github.com>

---
+++ app/assets/javascripts/application/posts.js
@@ -0,0 +1,54 @@
+function removePost(group_id, membership_id, post_type_id, post_id) {
+    const button = document.getElementById(`post-action-button-${membership_id}-${post_type_id}`)
+    button.disabled = true
+    fetch(`/groups/${group_id}/memberships/${membership_id}/posts/${post_id}.json`,
+        {
+            headers: {
+                'X-CSRF-TOKEN': getCsrfToken(),
+            },
+            method: 'DELETE',
+            credentials: 'same-origin',
+        })
+        .then(response => {
+            if (response.status === 200) {
+                button.onclick = () => addPost(group_id, membership_id, post_type_id)
+
+                button.classList.remove("uk-button-danger")
+                button.classList.add("uk-button-success")
+
+                const buttonTexts = button.innerText.split('(')
+                buttonTexts[1] = "(Hozzáad)"
+                button.innerText = buttonTexts.join('')
+                button.disabled = false
+            }
+        })
+}
+
+function addPost(group_id, membership_id, post_type_id) {
+    const button = document.getElementById(`post-action-button-${membership_id}-${post_type_id}`)
+    button.disabled = true
+    fetch(`/groups/${group_id}/memberships/${membership_id}/posts.json`, {
+        headers: {
+            'X-CSRF-TOKEN': getCsrfToken(),
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        credentials: 'same-origin',
+        body: JSON.stringify({post_type_id: post_type_id})
+    }).then(response => {
+        if (response.status === 200) {
+            response.json().then(data => {
+                button.onclick = () => removePost(group_id, membership_id, post_type_id, data.post_id)
+
+                button.classList.remove("uk-button-success")
+                button.classList.add("uk-button-danger")
+
+                const buttonTexts = button.innerText.split('(')
+                buttonTexts[1] = "(Elvesz)"
+                button.innerText = buttonTexts.join('')
+
+                button.disabled = false
+            });
+        }
+    })
+}
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,19 @@ class PostsController < ApplicationController
     @membership = Membership.find(params[:membership_id])
   end
 
+  def group_posts
+    @group = Group.includes(:post_types, :memberships).find(params[:group_id])
+    authorize @group, :manage_posts?
+
+    @post_type_ids = @group.post_types.map(&:id)
+                           .reject { |post_type_id| post_type_id.eql? PostType::LEADER_POST_ID }
+    @group_post_types = @group.post_types.reduce({}) do |hash, post_type|
+      hash.merge({ post_type.id => post_type })
+    end
+    @memberships = @group.memberships.active.includes(:posts, :user)
+                         .sort { |a, b| hu_compare(a.user.full_name, b.user.full_name) }
+  end
+
   def create
     group = Group.find(params[:group_id])
     membership = Membership.find(params[:membership_id])
@@ -12,17 +25,29 @@ class PostsController < ApplicationController
     authorize Post.new(membership: membership, post_type_id: post_type_id)
 
     new_post = CreatePost.call(group, membership, post_type_id)
-    return redirect_to group_path(group) if new_post.leader?
+    respond_to do |format|
+      format.html do
+        return redirect_to group_path(group) if new_post.leader?
 
-    redirect_back fallback_location: group_path(group)
+        redirect_back fallback_location: group_path(group)
+      end
+      format.json { render json: { post_id: new_post.id } }
+    end
   end
 
   def destroy
     post_id = params[:id]
     group_url = group_path(params[:group_id])
     authorize Post.find(post_id)
-    return redirect_back fallback_location: group_url if DestroyPost.call(post_id)
 
-    redirect_back fallback_location: group_url, alert: t(:no_leader_error)
+    destroyed = DestroyPost.call(post_id)
+    respond_to do |format|
+      format.html do
+        return redirect_back fallback_location: group_url if destroyed
+
+        redirect_back fallback_location: group_url, alert: t(:no_leader_error)
+      end
+      format.json { return render plain: 'ok', status: :ok }
+    end
   end
 end
