d6f11b25c878b88e87a2791b2d728fd848431f8c
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-12-18 20:05:02 +0100

    implement subgroup filter for evaluation table

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -34,30 +34,32 @@ class EvaluationsController < ApplicationController
 
   def table
     authorize_evaluation
-    @point_details = PointDetail.joins(point_request: :evaluation)
-                                .where(evaluations: { id: current_evaluation.id })
-                                .includes(:principle)
-    @evaluation = current_evaluation
-    @sub_groups = @evaluation.group.sub_groups
+    @point_details      = PointDetail.joins(point_request: :evaluation)
+                                     .where(evaluations: { id: current_evaluation.id })
+                                     .includes(:principle)
+    @evaluation         = current_evaluation
+    @sub_groups         = @evaluation.group.sub_groups
     @ordered_principles = @evaluation.principles.order(:type, :id)
-    #filter_principles
+    filter_principles
     point_eligible_user_ids = @evaluation.group.point_eligible_memberships.map(&:user_id)
-    @users = User.with_full_name.where(id: point_eligible_user_ids)
-                 .includes(:entry_requests,
-                           point_requests: [point_details: [:point_detail_comments, :principle]])
-
+    @users                  = User.with_full_name.where(id: point_eligible_user_ids)
+                                  .includes(:entry_requests,
+                                            point_requests: [point_details: %i[
+                                              point_detail_comments principle
+                                            ]])
 
     filter_users
     search_users
     @users = @users.order(:full_name).page(params[:page]).distinct
 
-    @users_for_pagination = @users
-    @users = EvaluationUserDecorator.decorate_collection(@users, context: { evaluation: @evaluation })
-    asd = @users.first.single_detail(@ordered_principles.first)
+    @users_for_pagination        = @users
+    @users                       = EvaluationUserDecorator.decorate_collection(@users,
+                                                                               context: { evaluation: @evaluation })
+
     @evaluation_point_calculator = EvaluationPointCalculator.new(@users)
-    evaluation_policy = policy(@evaluation)
-    @update_point_request = evaluation_policy.update_point_request?
-    @update_entry_request = evaluation_policy.update_entry_request?
+    evaluation_policy            = policy(@evaluation)
+    @update_point_request        = evaluation_policy.update_point_request?
+    @update_entry_request        = evaluation_policy.update_entry_request?
   end
 
   def submit_entry_request
